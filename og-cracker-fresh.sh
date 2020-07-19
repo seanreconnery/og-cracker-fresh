0d974998472198ae4f4373da3d4375d4cf3c9773
Sean RECONNery <56751730+seanreconnery@users.noreply.github.com>
2020-07-19 17:32:14 -0400

    accept list of images, saves output data to file

--- og-cracker-fresh.sh
+++ og-cracker-fresh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FILE=$1
-PWLIST=$2
+FILE=$1			# list of images to scan
+PWLIST=$2		# list of passwords to try on each image
 
 TMP_FILE=/tmp/out
 
@@ -16,7 +16,7 @@ NO_COLOR='\033[0m'
 check_result_file() {
   RESULT_FILE=$1
   PW=$2
-
+  IMG=$3
   if [ ! -f "$RESULT_FILE" ]; then
     echo "Nope.."
     echo ""
@@ -35,10 +35,18 @@ check_result_file() {
     echo -e "Password:  ${GREEN} $PW ${NO_COLOR}"
     echo -e "Result size: ${CYAN} $SIZE ${NO_COLOR}"
     echo -e "Type: ${BLUE} $TYPE ${NO_COLOR}"
-    echo ""
+    echo -e "Image:  ${GREEN}$IMG${NO_COLOR}"
     echo "-------------------------------------"
     echo ""
-    sleep 2
+
+	echo "--------------------------------------------------" >> fresh-results.txt
+	echo "Image:      $IMG" >> fresh-results.txt
+	echo "Password:   $PW" >> fresh-results.txt
+	echo "Type:       $TYPE" >> fresh-results.txt
+	echo "Size:       $SIZE" >> fresh-results.txt
+	echo "" >> fresh-results.txt
+
+    sleep 0.3301
   else
     echo -e "  ### ${MAG} Probably nothing ${NO_COLOR} | ${RED} data was found ${NO_COLOR} ###"
     echo -e "Result size:${CYAN} $SIZE ${NO_COLOR}( type: ${RED}$TYPE${NO_COLOR} )"
@@ -57,30 +65,45 @@ pws=$(wc -l < $PWLIST)
 echo "       Began at:  $now"
 echo "       With $pws passwords"
 echo ""
+echo "       Output Saved:  fresh-results.txt"
 echo ""
 
+
+
+
+while IFS= read -r animg
+do
+
+echo -e "Working on Image:  ${GREEN}$animg${NO_COLOR}"
+echo "------------------------------------------------------------------"
+echo ""
 
 while IFS= read -r line
 do
 	echo "Trying:  $line   (ECC off)"
 	echo "-------------------------------------"
-	outguess -r -k $line $FILE $TMP_FILE
-	check_result_file $TMP_FILE $line
+	outguess -r -k $line $animg $TMP_FILE
+	check_result_file $TMP_FILE $line $animg
 	sleep 0.1033
 
 	echo "Trying:  $line   (ECC on)"
 	echo "-------------------------------------"
-	outguess -r -e -k $line $FILE $TMP_FILE
-	check_result_file $TMP_FILE $line
+	outguess -r -e -k $line $animg $TMP_FILE
+	check_result_file $TMP_FILE $line $animg
 	echo ""
 	sleep 0.1033
 	echo ""
 
 done < "$2"
 
+
+done < "$FILE"
+
+
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "       Cracking Complete"
 now=$(date +"%T")
 echo "       Finished at:  $now"
 echo ""
+
 
